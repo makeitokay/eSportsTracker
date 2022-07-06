02e9c7c126043b69a3b6a7771465158133bb47d1
makeitokay <andrey.mu11@yandex.ru>
2022-07-06 21:04:55 +0300

    Initial commit: created a project

---
+++ eSportsTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace eSportsTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
