08ad573f3a1cbe1ca9fa1da5f47c24d5b5980c9d
makeitokay <andrey.mu11@yandex.ru>
2022-07-10 18:38:40 +0300

    Added default Identity system for the app

--- eSportsTracker.Web/Program.cs
+++ eSportsTracker.Web/Program.cs
@@ -1,7 +1,11 @@
 using eSportsTracker.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
@@ -24,8 +28,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
