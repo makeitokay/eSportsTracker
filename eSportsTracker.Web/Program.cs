14f0df1f2701824f42af784e85eaf8a6ccc671c7
makeitokay <andrey.mu11@yandex.ru>
2022-07-10 12:41:05 +0300

    Created application database context, connected to local MS SQL database

--- eSportsTracker.Web/Program.cs
+++ eSportsTracker.Web/Program.cs
@@ -1,4 +1,10 @@
+using eSportsTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
